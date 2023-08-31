42d323590d2feb82f375beec206c74d3f1fd3e7e
snowdot <co.pascu@gmail.com>
2023-08-31 14:45:32 +0200

    feat: add nav and social links to constants

---
+++ app/utils/constants.ts
@@ -0,0 +1,67 @@
+import { ILink } from "../types";
+
+export const NAV_LINKS: ILink[] = [
+  {
+    id: "about",
+    label: "About",
+    href: "/about",
+    hasDefaultTarget: true,
+  },
+  {
+    id: "blog",
+    label: "Blog",
+    href: "/blog",
+    hasDefaultTarget: true,
+  },
+  {
+    id: "supporters",
+    label: "Supporters",
+    href: "/supporters",
+    hasDefaultTarget: true,
+  },
+  {
+    id: "contribute",
+    label: "Contribute",
+    href: "https://geyser.fund/project/orange",
+    hasDefaultTarget: false,
+  },
+];
+
+export const SOCIAL_LINKS: ILink[] = [
+  {
+    id: "discord",
+    label: "Discord",
+    href: "https://dsc.gg/orangemart",
+    hasDefaultTarget: false,
+  },
+  {
+    id: "nostr",
+    label: "Nostr",
+    href: "https://satellite.earth/@orange@orangem.art",
+    hasDefaultTarget: false,
+  },
+  {
+    id: "tiktok",
+    label: "TikTok",
+    href: "https://www.tiktok.com/@theorangemart",
+    hasDefaultTarget: false,
+  },
+  {
+    id: "twitch",
+    label: "Twitch",
+    href: "https://www.twitch.tv/orangemart",
+    hasDefaultTarget: false,
+  },
+  {
+    id: "youtube",
+    label: "Youtube",
+    href: "https://www.youtube.com/@orangemart",
+    hasDefaultTarget: false,
+  },
+  {
+    id: "twitter",
+    label: "Twitter",
+    href: "https://twitter.com/theorangemart",
+    hasDefaultTarget: false,
+  },
+];
