9ab14da1d98fac421d95f8c43191e0973904ff2b
snowdot <co.pascu@gmail.com>
2023-09-01 16:06:10 +0200

    fix: correct title

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ import Footer from "./components/Footer";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "OrangeMart",
+  title: "Orange",
   description: "",
 };
 
