f2e07f6468c8c3608c96b40b07890c4ad043953a
snowdot <co.pascu@gmail.com>
2023-09-01 08:35:59 +0200

    feat: add LinkBtn component

---
+++ app/components/Buttons/LinkBtn/index.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import styles from "./styles.module.css";
+import { ILink } from "@/app/types";
+import Asset from "@/app/components/Asset";
+
+export const LinkBtn = (props: ILink) => {
+  const { id, href, label, hasDefaultTarget } = props;
+  const target = hasDefaultTarget ? "_self" : "_blank";
+
+  return (
+    <Link
+      key={id}
+      className={styles.linkBtn}
+      href={href}
+      target={target}
+      rel="noreferrer"
+    >
+      {label}
+      <Asset name="arrow-right" size={20} color="var(--darkest)" />
+    </Link>
+  );
+};
+
+export default LinkBtn;
