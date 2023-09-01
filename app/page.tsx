63f5b6543fd532c6fba9cc460fb183d768476b0d
snowdot <co.pascu@gmail.com>
2023-09-01 08:36:16 +0200

    feat: add about section to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
+import Image from "next/image";
 import styles from "./page.module.css";
 import PlayBtn from "./components/Buttons/PlayBtn";
+import LinkBtn from "./components/Buttons/LinkBtn";
 
 export default function Home() {
   return (
@@ -17,6 +19,38 @@ export default function Home() {
                 </p>
                 <PlayBtn />
               </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className={styles.aboutSection}>
+        <div className="wrapper">
+          <div className={styles.aboutWrapper}>
+            <div className={styles.aboutContent}>
+              <h2>We are Orange</h2>
+              <p>
+                Our objective is simple - we want to help people learn to
+                survive and thrive with Bitcoin. We recognize that Bitcoin is an
+                essential tool for survival and believe a great way to learn
+                about it is by playing with it. Join our community and take the
+                Bitcoin Repopulation Unit Survival Test (B.R.U.S.T)
+              </p>
+              <LinkBtn
+                id="join"
+                href="https://www.youtube.com/@orangemart"
+                label="Join community"
+                hasDefaultTarget={false}
+              />
+            </div>
+            <div className={styles.aboutImage}>
+              <Image
+                src="/assets/flag.png"
+                alt="flag"
+                className="cover"
+                width={400}
+                height={225}
+                priority
+              />
             </div>
           </div>
         </div>
