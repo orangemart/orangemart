6c71305cb4fb08370e57feeb6eee7da360b914be
snowdot <co.pascu@gmail.com>
2023-09-06 10:14:03 +0200

    fix: property name

--- app/components/Icons/GlobalIcon.tsx
+++ app/components/Icons/GlobalIcon.tsx
@@ -5,7 +5,7 @@ export function GlobalIcon({ color }: TIcon) {
 		<svg
 			stroke="currentColor"
 			fill="currentColor"
-			stroke-width="0"
+			strokeWidth="0"
 			viewBox="0 0 24 24"
 			height="1em"
 			width="1em"
