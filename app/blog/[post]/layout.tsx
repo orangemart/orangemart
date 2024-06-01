78e7762444408e6c5767f5450acac10016bd6412
ORANGEMART <orangemart@protonmail.com>
2024-06-02 11:26:34 +1200

    Update layout.tsx
    "This setup should ensure that when you link to a specific blog post, the correct Open Graph image (the blog post's hero image) is displayed, rather than the generic website banner image."

--- app/blog/[post]/layout.tsx
+++ app/blog/[post]/layout.tsx
@@ -2,39 +2,56 @@ import type { Metadata, ResolvingMetadata } from "next";
 import { getFileFromPath } from "@/app/utils/functions";
 
 type Props = {
-	params: { post: string };
+  params: { post: string };
 };
 
 export async function generateMetadata(
-	{ params }: Props,
-	parent: ResolvingMetadata,
+  { params }: Props,
+  parent: ResolvingMetadata,
 ): Promise<Metadata> {
-	const post = params.post;
-	const blogPost = await getFileFromPath(post);
-	const { frontmatter } = blogPost;
+  const post = params.post;
+  const blogPost = await getFileFromPath(post);
+  const { frontmatter } = blogPost;
 
-	if (frontmatter) {
-		const { title, description, heroImage } = frontmatter;
-		const previousImages = (await parent).openGraph?.images || [];
+  if (frontmatter) {
+    const { title, description, heroImage } = frontmatter;
+    const previousMetadata = await parent;
 
-		return {
-			title: title || "Post",
-			description: description || "",
-			openGraph: {
-				images: [heroImage, ...previousImages],
-			},
-		};
-	} else {
-		return {
-			title: "Post",
-			description: "",
-			openGraph: {
-				images: [],
-			},
-		};
-	}
+    return {
+      title: title || "Post",
+      description: description || "",
+      openGraph: {
+        title: title || "Post",
+        description: description || "",
+        images: heroImage ? [heroImage] : previousMetadata.openGraph?.images || [],
+        url: `https://www.orangem.art/blog/${post}`,
+      },
+      twitter: {
+        card: "summary_large_image",
+        title: title || "Post",
+        description: description || "",
+        images: heroImage ? [heroImage] : previousMetadata.twitter?.images || [],
+      },
+    };
+  } else {
+    return {
+      title: "Post",
+      description: "",
+      openGraph: {
+        title: "Post",
+        description: "",
+        images: [],
+      },
+      twitter: {
+        card: "summary_large_image",
+        title: "Post",
+        description: "",
+        images: [],
+      },
+    };
+  }
 }
 
 export default function PostLayout({ children }: { children: React.ReactNode }) {
-	return <>{children}</>;
+  return <>{children}</>;
 }
